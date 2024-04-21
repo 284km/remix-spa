53f72674b055f0839e318bb4068c5178c92a9fc7
284km <k.furuhashi10@gmail.com>
2024-04-21 19:23:26 +0900

    use github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
+
 export default defineConfig({
+  base: "/remix-spa/",
   plugins: [
     remix({
       ssr: false,
+      basename: "/remix-app/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html"),
+        );
+      }
     }),
     tsconfigPaths(),
   ],
